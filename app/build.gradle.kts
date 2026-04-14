cd01659fe56de72287406256ccd1efe5d1cace19
MTGMODS <bogdan.mtg@gmail.com>
2026-04-14 23:10:54 +0300

    Add navigation-compose dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.compose.material:material-icons-extended")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
