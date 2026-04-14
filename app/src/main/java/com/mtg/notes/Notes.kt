7abce72160c4612699dc0cf55adef3f0201fae5b
MTGMODS <bogdan.mtg@gmail.com>
2026-04-14 22:30:32 +0300

    Refactor Note class: remove deprecated edit method with single parameter for content

--- app/src/main/java/com/mtg/notes/Notes.kt
+++ app/src/main/java/com/mtg/notes/Notes.kt
@@ -32,11 +32,6 @@ open class Note(
         }
     }
 
-    open fun edit(newContent: String) {
-        this.content = newContent
-        this.updatedAt = System.currentTimeMillis()
-    }
-
     open fun edit(newTitle: String, newContent: String, newFolder: Folder?) {
         this.title = newTitle
         this.content = newContent
