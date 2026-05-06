0340a4782f3bd45a65c6cb332b3c31f63a5093d7
MTGMODS <bogdan.mtg@gmail.com>
2026-05-06 12:41:06 +0300

    Add updateNote API method to NoteApiService for note updates

--- app/src/main/java/com/mtg/notes/network/NoteApiService.kt
+++ app/src/main/java/com/mtg/notes/network/NoteApiService.kt
@@ -4,6 +4,7 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface NoteApiService {
@@ -19,4 +20,9 @@ interface NoteApiService {
 
     @DELETE("notes/{id}")
     suspend fun deleteNote(@Path("id") id: String)
+
+    @PUT("notes/{id}")
+    suspend fun updateNote(@Path("id") id: String, @Body note: NetworkNote): NetworkNote
+
+
 }
